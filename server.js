be3b9ab422f041816697c2b98cfe81adc6d55c37
Sadeepa Ranasinghe <sadeepar2001@gmail.com>
2026-06-12 08:11:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin:[ "http://localhost:5173",
       "http://wavepos.s3-website-ap-southeast-2.amazonaws.com",
-      "d1t4byt8d25y4p.cloudfront.net"
+      "https://d1t4byt8d25y4p.cloudfront.net"
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
